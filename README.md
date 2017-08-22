b7a321237f0327b01fdaa46c04a125a21e384933
emiliowd <eherna40@gmail.com>
2017-08-23 01:55:07 +0200

    Primer commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Animacion-planet
+
+Una representacion basica de una planeta y un cometa atravesando a traves de el
+Todo realizado con css y html
+
+Proyecto prueba para Curso animaciones web paltzi.com
+
+
+## Ejemplo Codepen.io
+
+[https://codepen.io/emilio2505/pen/QMmxzj](https://codepen.io/emilio2505/pen/OjEJBV)
+
+## Créditos
+
+- [Emilio Hernandez](https://www.linkedin.com/in/emilio-rafael-hernandez-perez-3a8bb540/)
+
+## Licencia
+
+[MIT](https://opensource.org/licenses/MIT)# PlanetWeight
---
+++ index.js
@@ -0,0 +1,16 @@
+ const $meteoro = document.getElementById('meteoro')
+ const $cabeza = document.getElementById('cabeza')
+ const $cola = document.getElementById('cola')
+      $meteoro.addEventListener('animationend', (event) => {
+        console.log(event);
+//          $cola.style.animationName = 'explosion'
+//          $cola.style.animationIterationCount = '0'
+//          $cola.style.animationDuration = '0'
+//          $cola.style.opacity= 0
+//
+//          $cabeza.style.animationName = 'explosion'
+//          $cabeza.style.animationDuration = '4s'
+//          $cabeza.style.animationIterationCount = '1'
+          
+        
+      })
